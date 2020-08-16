7f146fc74bc9bfeaeb64f51b053e1d761bceda2e
Gbubemi Makpokpomi <gbubemimakpokpomi@gmail.com>
2020-08-16 07:35:30 +0100

    added random subscribers

--- dashboard/admin.py
+++ dashboard/admin.py
@@ -1,3 +1,38 @@
 from django.contrib import admin
+from .models import UserSubscriber
+from django.urls import path
+from django.conf.urls import include, url
+from django.http import HttpResponseRedirect
+from django.contrib.admin.views.decorators import staff_member_required
 
 # Register your models here.
+@admin.register(UserSubscriber)
+class UserSubscriberAdmin(admin.ModelAdmin):
+    list_display = ("id", "username", "email", "created_at", "is_active")
+    ordering = ("-created_at",) 
+    list_filter = ("created_at",)             
+    change_list_template = 'admin/user_change_list.html'
+    
+    actions = ['make_inactive', 'make_active']
+
+    def make_inactive(self, request, queryset):
+        queryset.update(is_active=False)
+
+
+    def make_active(self, request, queryset):
+        queryset.update(is_active=True)
+
+
+    def get_urls(self):
+        urls = super(UserSubscriberAdmin, self).get_urls()
+        my_urls = [
+            path('sendemail/', self.send_email,  name="custom_view"),
+        ]
+        return my_urls + urls
+
+
+    def send_email(self, request):
+        hello =self.model.objects.values('email')
+        print(hello)
+        self.message_user(request, "Email sent")
+        return HttpResponseRedirect("../")
--- dashboard/models.py
+++ dashboard/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class UserSubscriber(models.Model):
+    username = models.CharField(max_length=100)
+    email = models.EmailField()
+    created_at = models.DateTimeField()
+    is_active = models.BooleanField(default=True)
